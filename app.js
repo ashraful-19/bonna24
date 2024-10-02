fc35c3c1d27caf601fffca3809598906c8be07c5
ashraful-19 <jlhs0177@gmail.com>
2024-10-03 04:17:47 +0600

    2nd changes after making express app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3005;
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/`);
+});
